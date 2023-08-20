e9ac603ed3da395dcf4c8cb153faf61b58af3df6
HaGa IT Solution <nengkya@yahoo.com>
2023-08-21 05:27:44 +0700

    register int register_ram = 20;

--- register_variable.c
+++ register_variable.c
@@ -13,9 +13,18 @@ int main() {
 		"- such as counters.\n"
 		"It should also be noted that defining 'register'\n"
 		"goes not mean that the variable will be stored in a register.\n"
-		"It means that it MIGHT be stored in a register - depending on hardware and implimentation restrictions.\n\n"
-	"Registers are faster than memory to access, so the variables which are most frequently used in a C program can be put in registers using the register keyword.  The keyword register hints to the compiler that a given variable can be put in a register. It’s the compiler’s choice to put it in a register or not. Generally, compilers themselves do optimizations and put the variables in a register.\n\n"
-"The keyword register hints to the compiler that a given variable can be put in a register. It’s the compiler’s choice to put it in a register or not. Generally, compilers themselves do optimizations and put the variables in a register.\n\n";
+		"It means that it MIGHT be stored in a register\n"
+		"- depending on hardware and implimentation restrictions.\n\n"
+
+		"Registers are faster than memory to access,\n"
+		"so the variables which are most frequently used in a C program\n"
+		"can be put in registers using the register keyword.\n\n"
+
+		"The keyword register hints to the compiler\n"
+		"that a given variable can be put in a register.\n"
+		"It’s the compiler’s choice to put it in a register or not.\n"
+		"Generally, compilers themselves do optimizations and put the variables in a register.\n\n";
+
 	register long register_ram = 10;
 	printf("\nregister long register_ram = %ld;\n\n", register_ram);
 	printf("%s", register_definition);
